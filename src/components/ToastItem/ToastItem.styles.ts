a1314b657427ab60cc4b7b2171b630b2c14aa104
Ilya758 <subscriber6436@gmail.com>
2022-04-12 23:31:49 +0300

    feat: implemented dynamic animation for the progressbar of created toasts

--- src/components/ToastItem/ToastItem.styles.ts
+++ src/components/ToastItem/ToastItem.styles.ts
@@ -1,9 +1,29 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { ToastItem } from './ToastItem';
 
 import success from '@images/svg/icon-success.svg';
 import iconClose from '@images/svg/icon-close.svg';
 import { COLORS } from '../../constants/colors';
+import { TProgressbarParams } from '../../models/toast';
+import { calculateAnimationParams } from '../../utils/calculateAnimationParams';
+
+const progressBarAnimation = (percents: number) => keyframes`
+  0% {
+    width: ${percents}%
+  }
+
+  100% {
+    width: 0%
+  }
+`;
+
+const toastAnimation = (lifetime: number) => {
+  const { percents, seconds } = calculateAnimationParams(lifetime);
+
+  return css`
+    ${seconds}s ${progressBarAnimation(percents)};
+  `;
+};
 
 export const ToastStyledItem = styled(ToastItem)`
   position: relative;
@@ -14,6 +34,7 @@ export const ToastStyledItem = styled(ToastItem)`
   font: 400 0.7rem Roboto, sans-serif;
   border-radius: 0.5rem;
   background-color: ${COLORS.success};
+  cursor: pointer;
 
   & .toast-top {
     &__info {
@@ -29,16 +50,23 @@ export const ToastStyledItem = styled(ToastItem)`
     right: 0.5rem;
     width: 0.8rem;
     height: 0.8rem;
-    background: url(${iconClose}) center/cover no-repeat;
+    background: url(${iconClose}) center/contain no-repeat;
     outline: none;
     border: none;
+    opacity: 0.6;
+    transition: 0.2s;
+
+    &:hover {
+      opacity: 1;
+      cursor: pointer;
+    }
   }
 
   & .icon-success {
     display: block;
     width: 1.8rem;
     height: 1.8rem;
-    background: url(${success}) center/cover no-repeat;
+    background: url(${success}) center/contain no-repeat;
   }
 
   & .toast {
@@ -46,13 +74,14 @@ export const ToastStyledItem = styled(ToastItem)`
       color: ${COLORS.light};
     }
   }
+`;
 
-  & .progressbar {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 0.2rem;
-    background-color: ${COLORS.light};
-  }
+export const Progressbar = styled.div<TProgressbarParams>`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 0%;
+  height: 0.2rem;
+  background-color: ${COLORS.light};
+  animation: ${({ lifetime }) => toastAnimation(lifetime)};
 `;
