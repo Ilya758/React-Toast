9a0ba0ed9279a6925f145f52b57a83c07cdc34f1
Ilya758 <subscriber6436@gmail.com>
2022-04-14 10:07:27 +0300

    fix: fixed types for component ToastItem

--- src/models/toast.ts
+++ src/models/toast.ts
@@ -1,11 +1,18 @@
+import { SetStateAction } from 'react';
+
 export type ToastConfig = {
   content: string;
 };
 
 export interface IAdditionalProps extends TProgressbarParams {
+  toasts: TGeneratedToast[];
+  phase: string;
   id: string;
-  dequeueCb: (timerId: string) => void;
+  dequeueCb: (timerId: string, removedByClick?: boolean) => void;
+  changeAnimCb: (id: string, toastPhase: string) => void;
 }
+
+export type TSetToastsCb = (value: SetStateAction<TGeneratedToast[]>) => void;
 
 export type TProgressbarParams = {
   lifetime: number;
